de9a30ce1a304d65ebfd2922a504907c64885221
Kyle Ellrott <kellrott@gmail.com>
2015-05-06 16:33:06 -0700

    Fixing website dependencies

--- bmegportal/bmeg_server.py
+++ bmegportal/bmeg_server.py
@@ -1,27 +1,35 @@
+#!/usr/bin/env python
+
 """bmeg_server.py:
 March 2014	chrisw
 
 A tornado server for submitting Groovy-flavored Gremlin query scripts to Rexster.
 
 """
-import BmegGremlinQueryHandler
-import BmegSigDbQueryHandler
+#import BmegGremlinQueryHandler
+#import BmegSigDbQueryHandler
 
+import os
 import sys
 import tornado.ioloop
 import tornado.web
 import tornado.autoreload
 
+def get_abspath(path):
+    return os.path.abspath(os.path.join(os.path.dirname(__file__), path))
+
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-  		self.write("Hello, world.  This is the MainHandler.")
+        self.render(get_abspath("../static/index.html"))
 
 # map urls to handlers
 application = tornado.web.Application([
-	(r"/", MainHandler),
-	(r"/static/(.*)", tornado.web.StaticFileHandler, {"path": r"static/"}),
-	(r"/query", BmegGremlinQueryHandler.BmegGremlinQueryHandler),
-	(r"/sigQuery", BmegSigDbQueryHandler.BmegSigDbQueryHandler)
+	#(r"/", MainHandler),
+    (r"/()", tornado.web.StaticFileHandler, {"path": get_abspath("../static/"), "default_filename": "index.html"}),
+	(r"/static/(.*)", tornado.web.StaticFileHandler, {"path": get_abspath("../static/")}),
+	(r"/images/(.*)", tornado.web.StaticFileHandler, {"path": get_abspath("../static/images/")}),
+	#(r"/query", BmegGremlinQueryHandler.BmegGremlinQueryHandler),
+	#(r"/sigQuery", BmegSigDbQueryHandler.BmegSigDbQueryHandler)
 ])
 
 # start server
